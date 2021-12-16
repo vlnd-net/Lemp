cd15722ab9e0528ae88e96257cc1a0400125c4fc
BambooLnd <86150531+BambooLnd@users.noreply.github.com>
2021-12-16 22:48:52 +0000

    Update varnish.sh

--- varnish.sh
+++ varnish.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
    curl -s https://packagecloud.io/install/repositories/varnishcache/varnish65/script.rpm.sh | bash 
-   echo
+   
    dnf -y install varnish
    rpm  --quiet -q varnish
 
- if [ "$?" = 0 ]; then
-   echo
    wget -qO /etc/systemd/system/varnish.service https://raw.githubusercontent.com/magenx/Magento-2-server-installation/master/varnish.service
    wget -qO /etc/varnish/varnish.params https://raw.githubusercontent.com/magenx/Magento-2-server-installation/master/varnish.params
    uuidgen > /etc/varnish/secret
