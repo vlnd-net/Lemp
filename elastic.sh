764d909dbb7a9326fd09aa4beb321fd7ce390989
BambooLnd <86150531+BambooLnd@users.noreply.github.com>
2021-12-16 23:07:04 +0000

    Create elastic.sh

---
+++ elastic.sh
@@ -0,0 +1,198 @@
+#!/bin/bash
+
+
+# Config path
+MAGENX_CONFIG_PATH="/opt/magenx/config"
+
+###################################################################################
+###                            DEFINE LINKS AND PACKAGES                        ###
+###################################################################################
+
+# ELK version lock
+ELKREPO="7.x"
+
+# Magento
+MAGE_VERSION="2"
+MAGE_VERSION_FULL=$(curl -s https://api.github.com/repos/magento/magento${MAGE_VERSION}/tags 2>&1 | head -3 | grep -oP '(?<=")\d.*(?=")')
+
+# Repositories
+MARIADB_VERSION="10.3"
+REPO_MARIADB_CFG="https://downloads.mariadb.com/MariaDB/mariadb_repo_setup"
+REPO_REMI_RPM="http://rpms.famillecollet.com/enterprise/remi-release-8.rpm"
+
+# WebStack Packages
+EXTRA_PACKAGES_DEB="curl jq gnupg2 auditd apt-transport-https apt-show-versions ca-certificates lsb-release make autoconf snapd automake libtool uuid-runtime \
+perl openssl unzip recode ed e2fsprogs screen inotify-tools iptables smartmontools clamav mlocate vim wget sudo bc apache2-utils \
+logrotate git python3-pip python3-dateutil python3-dev patch ipset strace rsyslog geoipupdate moreutils lsof xinetd jpegoptim sysstat acl attr iotop expect webp imagemagick snmp"
+PERL_MODULES_DEB="liblwp-protocol-https-perl libdbi-perl libconfig-inifiles-perl libdbd-mysql-perl  libterm-readkey-perl"
+PHP_PACKAGES_DEB=(cli fpm json common mysql zip lz4 gd mbstring curl xml bcmath intl ldap soap oauth apcu)
+
+EXTRA_PACKAGES_RPM="autoconf snapd jq automake dejavu-fonts-common dejavu-sans-fonts libtidy libpcap libwebp gettext-devel recode gflags tbb ed lz4 libyaml libdwarf \
+bind-utils e2fsprogs svn screen gcc iptraf inotify-tools iptables smartmontools net-tools mlocate unzip vim wget curl sudo bc mailx clamav-filesystem clamav-server \
+clamav-update clamav-milter-systemd clamav-data clamav-server-systemd clamav-scanner-systemd clamav clamav-milter clamav-lib logrotate git netcat patch ipset strace rsyslog \
+ncurses-devel GeoIP GeoIP-devel s3cmd geoipupdate openssl-devel ImageMagick libjpeg-turbo-utils pngcrush jpegoptim moreutils lsof net-snmp net-snmp-utils xinetd \
+python3-virtualenv python3-wheel-wheel python3-pip python3-devel ncftp postfix augeas-libs libffi-devel mod_ssl dnf-automatic sysstat libuuid-devel uuid-devel acl attr \
+iotop expect unixODBC gcc-c++"
+PHP_PACKAGES_RPM=(cli common fpm opcache gd curl mbstring bcmath soap mcrypt mysqlnd pdo xml xmlrpc intl gmp gettext-gettext phpseclib recode \
+symfony-class-loader symfony-common tcpdf tcpdf-dejavu-sans-fonts tidy snappy ldap lz4) 
+PHP_PECL_PACKAGES_RPM=(pecl-redis pecl-lzf pecl-geoip pecl-zip pecl-memcache pecl-oauth pecl-apcu)
+PERL_MODULES_RPM=(LWP-Protocol-https Config-IniFiles libwww-perl CPAN Template-Toolkit Time-HiRes ExtUtils-CBuilder ExtUtils-Embed ExtUtils-MakeMaker \
+TermReadKey DBI DBD-MySQL Digest-HMAC Digest-SHA1 Test-Simple Moose Net-SSLeay devel)
+
+# Nginx extra configuration
+REPO_MAGENX_TMP="https://raw.githubusercontent.com/magenx/Magento-2-server-installation/master/"
+NGINX_VERSION=$(curl -s http://nginx.org/en/download.html | grep -oP '(?<=gz">nginx-).*?(?=</a>)' | head -1)
+NGINX_BASE="https://raw.githubusercontent.com/magenx/Magento-nginx-config/master/"
+GITHUB_REPO_API_URL="https://api.github.com/repos/magenx/Magento-nginx-config/contents/magento2"
+
+# Debug Tools
+MYSQL_TUNER="https://raw.githubusercontent.com/major/MySQLTuner-perl/master/mysqltuner.pl"
+MYSQL_TOP="https://raw.githubusercontent.com/magenx/Magento-mysql/master/mytop"
+
+# Malware detector
+MALDET="https://www.rfxn.com/downloads/maldetect-current.tar.gz"
+
+###################################################################################
+###                                    COLORS                                   ###
+###################################################################################
+
+RED="\e[31;40m"
+GREEN="\e[32;40m"
+YELLOW="\e[33;40m"
+WHITE="\e[37;40m"
+BLUE="\e[0;34m"
+### Background
+DGREYBG="  \e[100m"
+BLUEBG="  \e[1;44m"
+REDBG="  \e[41m"
+### Styles
+BOLD="\e[1m"
+### Reset
+RESET="\e[0m"
+
+###################################################################################
+###                            ECHO MESSAGES DESIGN                             ###
+###################################################################################
+WHITETXT () {
+        MESSAGE=${@:-"${RESET}Error: No message passed"}
+        echo -e "  ${WHITE}${MESSAGE}${RESET}"
+}
+BLUETXT () {
+        MESSAGE=${@:-"${RESET}Error: No message passed"}
+        echo -e "  ${BLUE}${MESSAGE}${RESET}"
+}
+REDTXT () {
+        MESSAGE=${@:-"${RESET}Error: No message passed"}
+        echo -e "  ${RED}${MESSAGE}${RESET}"
+}
+GREENTXT () {
+        MESSAGE=${@:-"${RESET}Error: No message passed"}
+        echo -e "  ${GREEN}${MESSAGE}${RESET}"
+}
+YELLOWTXT () {
+        MESSAGE=${@:-"${RESET}Error: No message passed"}
+        echo -e "  ${YELLOW}${MESSAGE}${RESET}"
+}
+BLUEBG () {
+        MESSAGE=${@:-"${RESET}Error: No message passed"}
+        echo -e "${BLUEBG}${MESSAGE}${RESET}"
+}
+pause () {
+   read -p "  $*"
+}
+include_config () {
+    [[ -f "$1" ]] && . "$1"
+}
+_echo () {
+  echo -en "  $@"
+}
+
+###################################################################################
+###                            ARROW KEYS UP/DOWN MENU                          ###
+###################################################################################
+
+updown_menu () {
+i=1;for items in $(echo $1); do item[$i]="${items}"; let i=$i+1; done
+i=1
+echo -e "\n---> Use up/down arrow keys then press [ Enter ] to select $2"
+while [ 0 ]; do
+  if [ "$i" -eq 0 ]; then i=1; fi
+  if [ ! "${item[$i]}" ]; then let i=i-1; fi
+  echo -en "\r                                 " 
+  echo -en "\r${item[$i]}"
+  read -sn 1 selector
+  case "${selector}" in
+    "B") let i=i+1;;
+    "A") let i=i-1;;
+    "") echo; read -sn 1 -p "To confirm [ ${item[$i]} ] press "$(echo -e $BOLD$GREEN"y"$RESET)" or "$(echo -e $BOLD$RED"n"$RESET)" for new selection" confirm
+      if [[ "${confirm}" =~ ^[Yy]$  ]]; then
+        printf -v "$2" '%s' "${item[$i]}"
+        break
+      else
+        echo
+        echo -e "\n---> Use up/down arrow keys then press [ Enter ] to select $2"
+      fi
+      ;;
+  esac
+done }
+
+clear
+
+###################################################################################
+###                                        RUN IT                               ###
+###################################################################################
+
+GREENTXT "Elasticsearch installation:"
+echo
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+cat > /etc/yum.repos.d/elastic.repo << EOF
+[elasticsearch-${ELKREPO}]
+name=Elasticsearch repository for ${ELKREPO} packages
+baseurl=https://artifacts.elastic.co/packages/${ELKREPO}/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOF
+echo
+   dnf -y -q install --enablerepo=elasticsearch-${ELKREPO} elasticsearch kibana
+   rpm  --quiet -q elasticsearch
+          echo
+echo "discovery.type: single-node" >> /etc/elasticsearch/elasticsearch.yml
+echo "xpack.security.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
+sed -i "s/.*cluster.name.*/cluster.name: magento/" /etc/elasticsearch/elasticsearch.yml
+sed -i "s/.*node.name.*/node.name: magento-node1/" /etc/elasticsearch/elasticsearch.yml
+sed -i "s/.*network.host.*/network.host: 127.0.0.1/" /etc/elasticsearch/elasticsearch.yml
+sed -i "s/.*http.port.*/http.port: 9200/" /etc/elasticsearch/elasticsearch.yml
+sed -i "s/.*-Xms.*/-Xms512m/" /etc/elasticsearch/jvm.options
+sed -i "s/.*-Xmx.*/-Xmx2048m/" /etc/elasticsearch/jvm.options
+
+sed -i "s,#JAVA_HOME=,JAVA_HOME=/usr/share/elasticsearch/jdk/," /etc/sysconfig/elasticsearch
+chown -R :elasticsearch /etc/elasticsearch/*
+systemctl daemon-reload
+systemctl enable elasticsearch.service
+systemctl restart elasticsearch.service
+/usr/share/elasticsearch/bin/elasticsearch-setup-passwords auto -b > /tmp/elasticsearch
+
+cat > ${MAGENX_CONFIG_PATH}/elasticsearch <<END
+APM_SYSTEM_PASSWORD="$(awk '/PASSWORD apm_system/ { print $4 }' /tmp/elasticsearch)"
+KIBANA_SYSTEM_PASSWORD="$(awk '/PASSWORD kibana_system/ { print $4 }' /tmp/elasticsearch)"
+KIBANA_PASSWORD="$(awk '/PASSWORD kibana =/ { print $4 }' /tmp/elasticsearch)"
+LOGSTASH_SYSTEM_PASSWORD="$(awk '/PASSWORD logstash_system/ { print $4 }' /tmp/elasticsearch)"
+BEATS_SYSTEM_PASSWORD="$(awk '/PASSWORD beats_system/ { print $4 }' /tmp/elasticsearch)"
+REMOTE_MONITORING_USER_PASSWORD="$(awk '/PASSWORD remote_monitoring_user/ { print $4 }' /tmp/elasticsearch)"
+ELASTIC_PASSWORD="$(awk '/PASSWORD elastic/ { print $4 }' /tmp/elasticsearch)"
+END
+
+echo
+echo
+echo
+echo 
+GREENTXT "~    REPOSITORIES AND PACKAGES INSTALLATION IS COMPLETED    ~"
+WHITETXT "-------------------------------------------------------------------------------------"
+echo
+echo
+pause '[] Press [Enter] key to show the menu'
+printf "\033c"
+;;
